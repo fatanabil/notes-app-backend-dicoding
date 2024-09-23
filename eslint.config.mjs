77a1b537a1cd8b671b148e4eeddc932eb164e23c
fatanabil <fatanabil63@gmail.com>
2024-09-23 18:49:12 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+export default [
+  daStyle,
+  {
+    rules: {
+      'linebreak-style': ['error', 'windows'],
+    }
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
